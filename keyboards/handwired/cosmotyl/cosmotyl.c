d323a06841db3fe526bd421ca291fdb288901a24
felirx <felirx@gmail.com>
2024-08-15 17:36:58 +0300

    Debug spew

--- keyboards/handwired/cosmotyl/cosmotyl.c
+++ keyboards/handwired/cosmotyl/cosmotyl.c
@@ -15,3 +15,11 @@ report_mouse_t pointing_device_task_user(report_mouse_t report) {
     pmw33xx_set_cpi(0, PMW33XX_CPI);
     return report;
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+  return true;
+}
