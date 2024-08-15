9fda19684e325731b064ccc9094a642a2476b31a
felirx <felirx@gmail.com>
2024-08-15 17:20:47 +0300

    keymap changes

--- keyboards/handwired/cosmotyl/keymaps/default/keymap.c
+++ keyboards/handwired/cosmotyl/keymaps/default/keymap.c
@@ -13,12 +13,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT(
         KC_F12,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,                          KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
         KC_GRAVE, KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_LGUI,    LOWER,    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_EQUAL,
-        KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_BTN3,    XXXXXXX,  KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLASH,
-        KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_BTN2,    KC_DEL,   KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,
+        KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,     KC_BTN2,    KC_BTN3,  KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_SLASH,
+        KC_ESC,   KC_A,     KC_O,     KC_E,     KC_U,     KC_I,     KC_BTN1,    KC_DEL,   KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,
         KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,                           KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     KC_RSFT,
-        XXXXXXX,  KC_BTN2,  KC_BTN1,  KC_LALT,                                                                KC_RALT,  KC_BTN1,  KC_BTN2,  KC_RCTL,
-                                      KC_LCTL,  KC_SPC,   KC_LSFT,                                            RAISE,
-                                                KC_BTN2,  KC_BTN1,                        KC_6,     KC_RSFT,  KC_BSPC
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LALT,                                                                KC_RALT,  XXXXXXX,  XXXXXXX,  KC_RCTL,
+                                      KC_LCTL,  KC_SPC,   KC_LALT,                                            RAISE,
+                                                XXXXXXX,  XXXXXXX,                        KC_BTN1,  KC_LGUI,  KC_BSPC
     ),
 
     [_LOWER] = LAYOUT(
